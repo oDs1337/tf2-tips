5ab053aeb91820fcc1987a49f1991e3d721a1b7e
oDs <odstobiaszcod@gmail.com>
2022-05-22 17:48:48 +0200

    refactored databse of tips

---
+++ src/app/database/tipsPL.ts
@@ -0,0 +1,5 @@
+export const tipsPL = [
+  "Skaut może wykonać podwójny skok, aby zmienić kierunek i uniknąć ostrzału nieprzyjaciela.",
+  "Skaut zdobywa punkty kontrolne i popycha wózki z ładunkiem dwukrotnie szybciej niż inne klasy.",
+  "Skaut jest najskuteczniejszy, gdy pozostaje w ruchu i wykorzystuje swoją szybkość.",
+];
--- src/app/tips.reducer.ts
+++ src/app/tips.reducer.ts
@@ -1,28 +1,20 @@
+import { tipsPL } from './database/tipsPL';
+import { tipsEN } from './database/tipsEN';
 import { Action } from "@ngrx/store";
 
 export function tipsReducer(state: string = '', action: Action){
   console.log(action.type, state);
 
-  let englishTips = [
-    "As a Scout, jump while in mid-air to change direction and avoid enemy fire.",
-    "As a Scout, you capture Control Points and push Payload carts twice as fast as other classes.",
-    "As a Scout, you're most effective when you stay moving and use your speed to your advantage.",
-  ];
-  let polishTips = [
-    "Skaut może wykonać podwójny skok, aby zmienić kierunek i uniknąć ostrzału nieprzyjaciela.",
-    "Skaut zdobywa punkty kontrolne i popycha wózki z ładunkiem dwukrotnie szybciej niż inne klasy.",
-    "Skaut jest najskuteczniejszy, gdy pozostaje w ruchu i wykorzystuje swoją szybkość.",
-  ];
-
   const min = Math.ceil(0);
-  const max = Math.floor(englishTips.length);
+  const maxEN = Math.floor(tipsEN.length);
+  const maxPL = Math.floor(tipsPL.length);
 
   switch (action.type) {
     case 'ENGLISH':
-      return state = englishTips[Math.floor(Math.random() * (max - min)) + min];
+      return state = tipsEN[Math.floor(Math.random() * (maxEN - min)) + min];
 
     case 'POLISH':
-      return state = polishTips[Math.floor(Math.random() * (max - min)) + min];
+      return state = tipsPL[Math.floor(Math.random() * (maxPL - min)) + min];
 
     default:
       return state;
