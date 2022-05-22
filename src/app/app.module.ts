32bb374b1f662cdd3919899522151717ff071511
oDs <odstobiaszcod@gmail.com>
2022-05-22 15:22:03 +0200

    generated toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent
   ],
   imports: [
     BrowserModule,
