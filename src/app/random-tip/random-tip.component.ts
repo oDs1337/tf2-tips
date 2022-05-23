8ce9eb066ef6065dd40d5b886d7e6ec36c493577
oDs <odstobiaszcod@gmail.com>
2022-05-23 15:36:34 +0200

    working version before code review

--- src/app/random-tip/random-tip.component.ts
+++ src/app/random-tip/random-tip.component.ts
@@ -12,6 +12,8 @@ import { Character } from '../character';
 })
 export class RandomTipComponent implements OnInit {
 
+  test=false;
+
   tip$?: Observable<string>;
 
   currentCharacter?: any
@@ -20,11 +22,21 @@ export class RandomTipComponent implements OnInit {
     this.store.select('tip').subscribe(tip => {
       this.currentCharacter = tip;
     })
-
-    this.tip$ = this.store.select('tip');
    }
 
   ngOnInit(): void {
+    this.getRandomTip();
+  }
+
+  getRandomTip(): void{
+    const min = Math.ceil(0);
+    const max = Math.floor(characterNames.length);
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    this.store.dispatch({ type: characterNames[randomNumber] });
+  }
+
+  getAnotherCharacterTip(name: string){
+    this.store.dispatch({ type: name });
   }
 
 }
